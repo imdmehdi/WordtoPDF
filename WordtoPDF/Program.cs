8c56d3eaed855722ed86134fc4b6cceba528e584
imdmehdi <imdmehdi@hotmail.com>
2022-10-01 12:52:01 +0530

    List of path

--- WordtoPDF/Program.cs
+++ WordtoPDF/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using iText.Layout;
 using Microsoft.Office.Interop.Word;//C:\Windows\assembly\GAC_MSIL\Microsoft.Office.Interop.Word\15.0.0.0__71e9bce111e9429cSS    Add as COM
 using iText.Kernel.Utils;
+using iText.Kernel.Geom;
 
 Console.WriteLine("Hello, World!");
 
@@ -12,11 +13,11 @@ var appWord = new Application();
 if (appWord.Documents != null)
 {
     //yourDoc is your word document
-    var path1 = AppDomain.CurrentDomain.BaseDirectory;
-    var path2 = Environment.CurrentDirectory + "\\1.docx";
-    var wordDocument = appWord.Documents.Open(path2);
+    //var path1 = AppDomain.CurrentDomain.BaseDirectory;
+    var path2 = Environment.CurrentDirectory ;
+    var wordDocument = appWord.Documents.Open(path2 + "\\1.doc");
     //wordDocument.SelectContentControlsByTitle("FIRLD")[1].Range.Text = "VALIETOBIND";//IF BIND DYNAMIC CONTENT
-    string pdfDocName = path1 + "\\pdfDocument.pdf";
+    string pdfDocName = path2 + "\\pdfDocument.pdf";
     if (wordDocument != null)
     {
         wordDocument.ExportAsFixedFormat(pdfDocName,
@@ -24,30 +25,27 @@ if (appWord.Documents != null)
         wordDocument.Close();
     }
     appWord.Quit();
-    ManipulatePdf(pdfDocName);
+    List<string> pathsObj = new List<string>();
+    pathsObj.Add(pdfDocName);
+    pathsObj.Add(path2 + "\\a1.pdf");
+    pathsObj.Add(path2 + "\\a0.pdf");
+    ManipulatePdf(pathsObj);
 }
 
 
- void ManipulatePdf(String dest)
+ void ManipulatePdf(List<string> pathsObj)
 {
     var path2 = Environment.CurrentDirectory ;
-
-    PdfDocument pdfDoc = new PdfDocument(new PdfWriter(path2+"\\2.pdf"));
-    PdfDocument cover0 = new PdfDocument(new PdfReader(dest));
-
-    PdfDocument cover = new PdfDocument(new PdfReader(path2+ "\\a1.pdf"));
-    PdfDocument resource = new PdfDocument(new PdfReader(path2 + "\\a0.pdf"));
-    
+    PdfDocument pdfDoc = new PdfDocument(new PdfWriter(path2+"\\2.pdf"));//ceates new file
     PdfMerger merger = new PdfMerger(pdfDoc);
-    merger.Merge(cover0, 1, cover0.GetNumberOfPages());
-    merger.Merge(cover, 1, cover.GetNumberOfPages());
-    merger.Merge(resource, 1, resource.GetNumberOfPages());
-
-    // Source documents can be closed implicitly after merging,
-    // by passing true to the setCloseSourceDocuments(boolean) method
-    cover.Close();
-    resource.Close();
-
+    foreach (var path in pathsObj)
+    {
+        PdfDocument cover = new PdfDocument(new PdfReader(path));
+        merger.Merge(cover, 1, cover.GetNumberOfPages());
+        // Source documents can be closed implicitly after merging,
+        // by passing true to the setCloseSourceDocuments(boolean) method
+        cover.Close();
+    }
     // The resultant pdf doc will be closed implicitly.
     merger.Close();
 }
